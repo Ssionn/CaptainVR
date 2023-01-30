49832e8d4dd624fb48ad756c277ce36200886168
Casper Kizewski <163021@student.horizoncollege.nl>
2023-01-30 15:40:36 +0100

    home page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/home');
+
+Route::get('/home', [HomeController::class, 'index']);
+
 
