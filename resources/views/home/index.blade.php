ee86102c983eb878fe40590c4148782e74bbad79
Casper Kizewski <163021@student.horizoncollege.nl>
2023-01-31 09:11:34 +0100

    test

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -4,7 +4,8 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Test</title>
+    @vite('resources/css/app.css')
+    <title>CaptainVR - Home</title>
 </head>
 <body>
 
