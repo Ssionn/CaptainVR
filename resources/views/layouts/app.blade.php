e4087fd32385083a9f55ece5582a96a0c21b6ed8
Casper Kizewski <163021@student.horizoncollege.nl>
2023-01-31 14:30:04 +0100

    Merge fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,11 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite('resources/css/app.css')
+<<<<<<< HEAD
     <title>@yield('title') - TheVRCoaches</title>
+=======
+    <title>@yield('title')</title>
+>>>>>>> fe03e03 (Footer made)
 </head>
 <body class="h-screen">
 
