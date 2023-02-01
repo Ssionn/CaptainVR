6fc400ac5121af15ff79d351ced5f15367eb10d1
Casper Kizewski <163021@student.horizoncollege.nl>
2023-02-01 11:05:34 +0100

    Footer improved, less text, better understanding

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,14 @@ module.exports = {
         "./resources/**/*.vue",
     ],
     theme: {
-        extend: {},
+        extend: {
+            zoom: {
+                '25': '0.25',
+                '50': '0.5',
+                '75': '0.75',
+                '100': '1',
+            }
+        },
     },
     plugins: [],
 }
