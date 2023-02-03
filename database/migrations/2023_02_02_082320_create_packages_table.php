3ab6363dd6ce47bbbbb3052da106470fcf2d48f5
Casper Kizewski <163021@student.horizoncollege.nl>
2023-02-03 09:26:22 +0100

    Part of the data loaded in

---
+++ database/migrations/2023_02_02_082320_create_packages_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('packages', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('academic_id');
+            $table->foreign('academic_id')->references('id')->on('academics');
+            $table->string('title')->unique();
+            $table->string('image');
+            $table->string('description');
+            $table->string('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('packages');
+    }
+};
